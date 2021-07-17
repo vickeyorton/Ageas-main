ed35231e7ba529b984e23d31bc6cacdfda114c7e
vickeyorton <vigneshmooorthi@gmail.com>
2021-07-17 16:17:42 +0530

    Stepper

--- src/Components/IncidentDetail/incidentDetail.js
+++ src/Components/IncidentDetail/incidentDetail.js
@@ -4,14 +4,15 @@ import LogoHeader from '../LogoHeader/LogoHeader';
 import { connect } from 'react-redux';
 import {GET_INCIDENT} from '../../Redux/actions';
 import { useHistory } from 'react-router-dom';
+import Stepper from './Stepper/Stepper';
 
 function Incident(props){
 
     const dateList = ["DD",1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
     const monthList = ["MM",1,2,3,4,5,6,7,8,9,10,11,12];
     const yearList = ["YYYY",1990,1991,1992,1993,1994,1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022,2023,2024,2025,2026,2027,2028];
-    const hourList =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];
-    const minuteList = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60];
+    const hourList =["H",1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];
+    const minuteList = ["M",1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60];
 
     const [date, setDate] =useState('');
     const [month, setMonth] =useState('');
@@ -47,6 +48,7 @@ function Incident(props){
     return (
         <div className="Incident-Container">
             <LogoHeader/>
+            <Stepper/>
             <div className="col-12">
                 <div className="Section-2">
                     <div className="Section-2-Header">
